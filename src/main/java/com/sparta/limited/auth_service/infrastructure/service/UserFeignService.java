47d86e344227e7698fbeceee936d3f310e6cd622
Imnotcoderdude <topsolo7@naver.com>
2025-04-23 00:18:11 +0900

    ♻️ refactor [#15] : FeignClient 예외 처리 - FeignClient 호출시 발생 가능성이 존재하는 예외를 잡아서 처리 - try-catch 사용

--- src/main/java/com/sparta/limited/auth_service/infrastructure/service/UserFeignService.java
+++ src/main/java/com/sparta/limited/auth_service/infrastructure/service/UserFeignService.java
@@ -9,8 +9,11 @@ import com.sparta.limited.auth_service.infrastructure.dto.request.UserCreateInte
 import com.sparta.limited.auth_service.infrastructure.dto.response.UserCreateInternalResponse;
 import com.sparta.limited.auth_service.infrastructure.dto.response.UserSearchInternalResponse;
 import com.sparta.limited.auth_service.infrastructure.feign.UserFeignClient;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -21,22 +24,38 @@ public class UserFeignService {
     public AuthSignupResponse createUser(AuthSignupRequest request, String password) {
         UserCreateInternalRequest userCreateRequest = AuthDtoMapper.toCreateUserInternalDto(request,
             password);
-        UserCreateInternalResponse userCreateResponse = userFeignClient.createUser(
-            userCreateRequest);
-        return AuthDtoMapper.toSignupExternalResponse(userCreateResponse);
+        try {
+            UserCreateInternalResponse userCreateResponse = userFeignClient.createUser(
+                userCreateRequest);
+            return AuthDtoMapper.toSignupExternalResponse(userCreateResponse);
+        } catch (FeignException e) {
+            if (e.status() == 409) {
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 사용자명 입니다.");
+            }
+            throw new RuntimeException("user-service의 사용자 생성 기능에 문제가 발생했습니다.");
+        }
+
     }
 
     public UserInfo searchUserByUsername(AuthLoginRequest request) {
-        UserSearchInternalResponse userSearchResponse = userFeignClient.searchUserByUsername(
-            request.getUsername());
-        return UserInfo.from(
-            userSearchResponse.getUserId(),
-            userSearchResponse.getUsername(),
-            userSearchResponse.getPassword(),
-            userSearchResponse.getRole(),
-            userSearchResponse.getGender(),
-            userSearchResponse.getAge(),
-            userSearchResponse.getAddress()
-        );
+        try {
+            UserSearchInternalResponse userSearchResponse = userFeignClient.searchUserByUsername(
+                request.getUsername());
+            return UserInfo.from(
+                userSearchResponse.getUserId(),
+                userSearchResponse.getUsername(),
+                userSearchResponse.getPassword(),
+                userSearchResponse.getRole(),
+                userSearchResponse.getGender(),
+                userSearchResponse.getAge(),
+                userSearchResponse.getAddress()
+            );
+        } catch (FeignException e) {
+            if (e.status() == 404) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다");
+            }
+            throw new RuntimeException("user-service의 username 기반 사용자 조회 기능에 문제가 발생했습니다.");
+        }
+
     }
 }
